9f79756a31ab86d7735b57d0f681cd69d80a776f
Toni Leino <n1leto01@students.oamk.fi>
2022-10-16 17:15:53 +0300

    Toivottavasti valmis tuote

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState, } from 'react';
 import './App.css';
+const URL = 'https://api.exchangerate.host/latest';
 
 function App() {
+  const [eur, setEur] = useState(0);
+  const [gbp, setPunta] = useState(0);
+  const [rate, setKurssi] = useState(0);
+
+  async function convert(e) {
+    e.preventDefault(e);
+    try {
+      const osoite = URL;
+      const vastaus = await fetch(osoite);
+
+
+      if (vastaus.ok) {
+        const json = await vastaus.json();
+        setKurssi(json.rates.GBP);
+        setPunta(eur * json.rates.GBP)
+      } else {
+        alert('Virhe hakiessa valuutta käännöstä.')
+      }
+    } catch (err) {
+      alert(err); 
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div id="container">
+      <form>
+        <div>
+          <label>Eurot</label>&nbsp;
+          <input type="number" step="0.01"
+            value={eur} onChange={e => setEur(e.target.value)} />
+          <output>{rate}</output>
+        </div>
+        <div>
+          <label>Punnat</label>
+          <output>{gbp.toFixed(2)} €</output>
+        </div>
+        <div>
+          <button onClick={convert}>Laske kurssi</button>
+        </div>
+      </form>
     </div>
   );
 }
